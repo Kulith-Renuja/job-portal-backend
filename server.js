e432fdba15d92a94b361c6b55fc28821e229b0ac
kulith_renuja <kulithx175@gmail.com>
2025-07-04 15:38:31 +0530

    create authentication

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+/*
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
@@ -30,3 +31,28 @@ process.on('unhandledRejection', (err, promise) => {
   console.log(`Error: ${err.message}`);
   server.close(() => process.exit(1));
 });
+*/
+
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+
+dotenv.config();
+connectDB();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use('/api/v1/auth', require('./routes/authRoutes'));
+
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));

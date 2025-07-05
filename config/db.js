9335a8578e0864099deabd9d43e3700574b88f58
kulith_renuja <kulithx175@gmail.com>
2025-07-05 13:37:45 +0530

    connect database and check login and registration using postman

--- config/db.js
+++ config/db.js
@@ -1,11 +1,13 @@
 const mongoose = require('mongoose');
+
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log('MongoDB Connected...');
-  } catch (err) {
-    console.error('Database connection error:', err.message);
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error('MongoDB connection failed:', error.message);
     process.exit(1);
   }
 };
+
 module.exports = connectDB;

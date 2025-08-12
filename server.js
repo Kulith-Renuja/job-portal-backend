fb4f5dce99f2cb0c1e99ee894f1de71349dc6dc6
kulith_renuja <kulithx175@gmail.com>
2025-08-12 12:35:08 +0530

    second commit

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ connectDB();
 const app = express();
 
 app.use(cors({
-  origin:  'http://localhost:3000',
+  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
 }));
 
 app.use(express.json());
